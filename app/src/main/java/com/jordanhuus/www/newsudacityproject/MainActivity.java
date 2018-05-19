dca5dcc19de76092c47ab71285f6c4b81f43b5e7
Jordan H <jordanhuusy@live.com>
2018-05-19 15:52:31 -0700

    added LoaderManager.LoaderCallbacks to MainPageFragment

--- app/src/main/java/com/jordanhuus/www/newsudacityproject/MainActivity.java
+++ app/src/main/java/com/jordanhuus/www/newsudacityproject/MainActivity.java
@@ -1,21 +1,11 @@
 package com.jordanhuus.www.newsudacityproject;
 
-import android.app.FragmentTransaction;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ListView;
 
-import java.util.ArrayList;
-
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<News>>{
+public class MainActivity extends AppCompatActivity{
     private static final int NEWS_LOADER_ID = 1;
     private String newsCategory;
     private LoaderManager loaderManager;
@@ -31,33 +21,6 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // Set ViewPager adapter
         ViewPager viewPager = findViewById(R.id.main_view_pager);
         viewPager.setAdapter(adapter);
-    }
-
-    private void updateUi(ArrayList<News> articles){
-        // List View adapter
-        NewsAdapter adapter = new NewsAdapter(this, R.layout.news_list_item, articles);
-
-        // Set adapter to List View
-        ListView newsListView = findViewById(R.id.news_items_list_view);
-        newsListView.setAdapter(adapter);
-    }
-
-    @NonNull
-    @Override
-    public Loader<ArrayList<News>> onCreateLoader(int id, @Nullable Bundle args) {
-        return new NewsLoader(this, newsCategory);
-    }
-
-    @Override
-    public void onLoadFinished(@NonNull Loader<ArrayList<News>> loader, ArrayList<News> data) {
-        if(data!=null) {
-            updateUi(data);
-        }
-    }
-
-    @Override
-    public void onLoaderReset(@NonNull Loader<ArrayList<News>> loader) {
-        updateUi(new ArrayList<News>());
     }
 }
 
--- app/src/main/java/com/jordanhuus/www/newsudacityproject/MainPageFragment.java
+++ app/src/main/java/com/jordanhuus/www/newsudacityproject/MainPageFragment.java
@@ -4,25 +4,69 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by jordanhuus on 5/19/2018.
  */
 
-public class MainPageFragment extends Fragment {
+public class MainPageFragment extends Fragment implements LoaderManager.LoaderCallbacks<ArrayList<News>>{
+    private LoaderManager loaderManager;
+    private static final int NEWS_LOADER_ID = 1;
+    private String newsCategory;
+    private View root;
 
     public static MainPageFragment newInstance(){
         return new MainPageFragment();
     }
 
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main, container, false);
+        root = inflater.inflate(R.layout.activity_main, container, false);
+
+        // Retrieve newsCategory from bundle
+        newsCategory = "politics"; /* Placeholder */
+
+        // Restart loader
+        loaderManager = getActivity().getSupportLoaderManager();
+        loaderManager.initLoader(NEWS_LOADER_ID, null, this);
+
+        return root;
     }
 
+    private void updateUi(ArrayList<News> articles){
+        // List View adapter
+        NewsAdapter adapter = new NewsAdapter(getActivity(), R.layout.news_list_item, articles);
 
+        // Set adapter to List View
+        ListView newsListView = root.findViewById(R.id.news_items_list_view);
+        newsListView.setAdapter(adapter);
+    }
+
+    @NonNull
+    @Override
+    public Loader<ArrayList<News>> onCreateLoader(int id, @Nullable Bundle args) {
+        return new NewsLoader(getActivity(), newsCategory);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<ArrayList<News>> loader, ArrayList<News> data) {
+        if(data!=null) {
+            updateUi(data);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<ArrayList<News>> loader) {
+        updateUi(new ArrayList<News>());
+    }
 }

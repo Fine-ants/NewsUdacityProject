cbe7c6be7be3bbd0e4efa6fbdebd5917ba0fb09d
Jordan H <jordanhuusy@live.com>
2018-05-19 14:25:52 -0700

    added onClick call-backs to temporary news category buttons

--- app/src/main/java/com/jordanhuus/www/newsudacityproject/MainActivity.java
+++ app/src/main/java/com/jordanhuus/www/newsudacityproject/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -13,14 +15,47 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<News>>{
     private static final int NEWS_LOADER_ID = 1;
     private String newsCategory;
+    private LoaderManager loaderManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Load news data
-        getSupportLoaderManager().initLoader(NEWS_LOADER_ID, null, this);
+        // Init LoaderManager
+        loaderManager = getSupportLoaderManager();
+        if(savedInstanceState!=null){
+            loaderManager.initLoader(NEWS_LOADER_ID, null, MainActivity.this);
+        }
+
+        // Setup category onClick
+        Button technology = findViewById(R.id.technology_button);
+        technology.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newsCategory = "technology";
+                loaderManager.restartLoader(NEWS_LOADER_ID, null, MainActivity.this);
+            }
+        });
+
+        Button politics = findViewById(R.id.politics_button);
+        politics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newsCategory = "politics";
+                loaderManager.restartLoader(NEWS_LOADER_ID, null, MainActivity.this);
+            }
+        });
+
+        Button business = findViewById(R.id.business_button);
+        business.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                newsCategory = "business";
+                loaderManager.restartLoader(NEWS_LOADER_ID, null, MainActivity.this);
+            }
+        });
+
     }
 
     private void updateUi(ArrayList<News> articles){
@@ -35,7 +70,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @NonNull
     @Override
     public Loader<ArrayList<News>> onCreateLoader(int id, @Nullable Bundle args) {
-        return new NewsLoader(this, "politics");
+        return new NewsLoader(this, newsCategory);
     }
 
     @Override

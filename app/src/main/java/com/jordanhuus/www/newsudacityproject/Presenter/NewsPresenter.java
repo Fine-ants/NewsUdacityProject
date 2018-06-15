eff098d281d323c890f30524a3ec0acac29409e9
Jordan H <jordanhuusy@live.com>
2018-06-15 10:05:04 -0700

    began adding MVP architecture

---
+++ app/src/main/java/com/jordanhuus/www/newsudacityproject/Presenter/NewsPresenter.java
@@ -0,0 +1,25 @@
+package com.jordanhuus.www.newsudacityproject.Presenter;
+
+import com.jordanhuus.www.newsudacityproject.MainActivity;
+
+/**
+ * Created by jordanhuus on 6/15/2018.
+ */
+
+public class NewsPresenter {
+
+    private MainActivity mainActivity;
+
+    public NewsPresenter(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+    }
+
+
+    /**
+     * @param newsCategory is the user-chosen category
+     */
+    public void chooseNewsCategory(String newsCategory, ){
+
+    }
+
+}
---
+++ app/src/main/java/com/jordanhuus/www/newsudacityproject/View/NewsInterface.java
@@ -0,0 +1,11 @@
+package com.jordanhuus.www.newsudacityproject.View;
+
+/**
+ * Created by jordanhuus on 6/15/2018.
+ */
+
+public interface NewsInterface {
+
+    public void openCategoriesFragment();
+    public void openMainPageFragment();
+}

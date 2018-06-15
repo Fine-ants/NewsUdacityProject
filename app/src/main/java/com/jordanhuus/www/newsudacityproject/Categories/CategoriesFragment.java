ce21b8703b918bc9c23538f662d60c11114427ff
Jordan H <jordanhuusy@live.com>
2018-06-15 16:57:59 -0700

    migrated the inflation of root view

--- app/src/main/java/com/jordanhuus/www/newsudacityproject/Categories/CategoriesFragment.java
+++ app/src/main/java/com/jordanhuus/www/newsudacityproject/Categories/CategoriesFragment.java
@@ -31,7 +31,6 @@ public class CategoriesFragment extends Fragment {
 
     private MainActivity mainActivity;
     private String chosenCategoryTag;
-    private ArrayList<Category> categories;
     private CategoriesPresenter presenter;
 
     public static CategoriesFragment newInstance(){
@@ -43,56 +42,7 @@ public class CategoriesFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.categories, container, false);
 
-        // Set onClickListener for Search button
-        Button searchNewsKeyword = root.findViewById(R.id.search_button);
-        searchNewsKeyword.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // @param view represents the button object; find parent view in order to retrieve EditText object
-                View root = (View) view.getParent();
-                EditText keywordSearch = root.findViewById(R.id.search_news_keyword);
-                String keyword = keywordSearch.getText().toString();
-
-                // Confirm device connectivity
-                ConnectivityManager connectivityManager = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-                if(connectivityManager.getActiveNetworkInfo()==null || !connectivityManager.getActiveNetworkInfo().isConnected()){
-                    return;
-                }
-
-                //
-                if(!keyword.isEmpty()){
-                    mainActivity.clickCategory("Articles", keyword, false);
-                }
-            }
-        });
-
-        categories = new ArrayList<>();
-        categories.add(new Category(getString(R.string.category_tech_tag),getString(R.string.category_tech),R.drawable.baseline_memory_white_18dp));
-        categories.add(new Category(getString(R.string.category_science_tag),getString(R.string.category_science),R.drawable.science_white_icon));
-        categories.add(new Category(getString(R.string.category_health_tag),getString(R.string.category_health),R.drawable.baseline_fitness_center_white_18dp));
-        categories.add(new Category(getString(R.string.category_weather_tag),getString(R.string.category_weather),R.drawable.baseline_cloud_white_18dp));
-        categories.add(new Category(getString(R.string.category_business_tag),getString(R.string.category_business),R.drawable.baseline_business_white_18dp));
-        categories.add(new Category(getString(R.string.category_politics_tag),getString(R.string.category_politics),R.drawable.baseline_account_balance_white_18dp));
-        categories.add(new Category(getString(R.string.category_sports_tag),getString(R.string.category_sports),R.drawable.baseline_directions_bike_white_18dp));
-        categories.add(new Category(getString(R.string.category_world_tag),getString(R.string.category_world),R.drawable.baseline_public_white_18dp));
-        categories.add(new Category(getString(R.string.category_entertainment_tag),getString(R.string.category_entertainment),R.drawable.baseline_local_movies_white_18dp));
-
-        // Setup Categories ListView
-        CategoriesAdapter adapter = new CategoriesAdapter(getContext(), R.layout.category_list_item, categories);
-        ListView categoriesList = root.findViewById(R.id.categories_list_view);
-        categoriesList.setAdapter(adapter);
-        categoriesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                // Retrieve the chosen category title, api category tag
-                String chosenCategoryTitle = categories.get(i).getCategoryTitle();
-                String chosenCategoryTag = categories.get(i).getCategoryApiTag();
-
-                // Pass parameters to ArticlesFragment
-                mainActivity.clickCategory(chosenCategoryTitle, chosenCategoryTag, true);
-            }
-        });
+        presenter.inflateCategoriesView(root);
 
         return root;
     }
@@ -101,6 +51,7 @@ public class CategoriesFragment extends Fragment {
     private class CustomOnclickListener implements View.OnClickListener{
         @Override
         public void onClick(View view) {
+            // Retrieve chosen category title and it's tag
             chosenCategoryTag = view.getTag().toString();
             Button button = (Button) view;
             String chosenCategoryTitle = button.getText().toString();
--- app/src/main/java/com/jordanhuus/www/newsudacityproject/Categories/Presenter/CategoriesPresenter.java
+++ app/src/main/java/com/jordanhuus/www/newsudacityproject/Categories/Presenter/CategoriesPresenter.java
@@ -2,10 +2,20 @@ package com.jordanhuus.www.newsudacityproject.Categories.Presenter;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.jordanhuus.www.newsudacityproject.Categories.CategoriesAdapter;
 import com.jordanhuus.www.newsudacityproject.Categories.CategoriesFragment;
+import com.jordanhuus.www.newsudacityproject.Categories.Model.Category;
 import com.jordanhuus.www.newsudacityproject.MainActivity;
+import com.jordanhuus.www.newsudacityproject.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by jordanhuus on 6/15/2018.
@@ -14,6 +24,7 @@ import com.jordanhuus.www.newsudacityproject.MainActivity;
 public class CategoriesPresenter {
     private CategoriesFragment categoriesFragment;
     private MainActivity mainActivity;
+    private ArrayList<Category> categories;
 
     public CategoriesPresenter(CategoriesFragment categoriesFragment) {
         this.categoriesFragment = categoriesFragment;
@@ -32,6 +43,61 @@ public class CategoriesPresenter {
 
         // Pass the chosen news category to ArticlesFragment
         mainActivity.clickCategory(chosenCategoryTitle, chosenCategoryTag, true);
+    }
+
+    public View inflateCategoriesView(View rootView){
+        // Set onClickListener for Search button
+        Button searchNewsKeyword = rootView.findViewById(R.id.search_button);
+        searchNewsKeyword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // @param view represents the button object; find parent view in order to retrieve EditText object
+                View root = (View) view.getParent();
+                EditText keywordSearch = root.findViewById(R.id.search_news_keyword);
+                String keyword = keywordSearch.getText().toString();
+
+                // Confirm device connectivity
+                ConnectivityManager connectivityManager = (ConnectivityManager) mainActivity.getSystemService(Context.CONNECTIVITY_SERVICE);
+                if(connectivityManager.getActiveNetworkInfo()==null || !connectivityManager.getActiveNetworkInfo().isConnected()){
+                    return;
+                }
+
+                //
+                if(!keyword.isEmpty()){
+                    mainActivity.clickCategory("Articles", keyword, false);
+                }
+            }
+        });
+
+        categories = new ArrayList<>();
+        categories.add(new Category(mainActivity.getString(R.string.category_tech_tag),mainActivity.getString(R.string.category_tech),R.drawable.baseline_memory_white_18dp));
+        categories.add(new Category(mainActivity.getString(R.string.category_science_tag),mainActivity.getString(R.string.category_science),R.drawable.science_white_icon));
+        categories.add(new Category(mainActivity.getString(R.string.category_health_tag),mainActivity.getString(R.string.category_health),R.drawable.baseline_fitness_center_white_18dp));
+        categories.add(new Category(mainActivity.getString(R.string.category_weather_tag),mainActivity.getString(R.string.category_weather),R.drawable.baseline_cloud_white_18dp));
+        categories.add(new Category(mainActivity.getString(R.string.category_business_tag),mainActivity.getString(R.string.category_business),R.drawable.baseline_business_white_18dp));
+        categories.add(new Category(mainActivity.getString(R.string.category_politics_tag),mainActivity.getString(R.string.category_politics),R.drawable.baseline_account_balance_white_18dp));
+        categories.add(new Category(mainActivity.getString(R.string.category_sports_tag),mainActivity.getString(R.string.category_sports),R.drawable.baseline_directions_bike_white_18dp));
+        categories.add(new Category(mainActivity.getString(R.string.category_world_tag),mainActivity.getString(R.string.category_world),R.drawable.baseline_public_white_18dp));
+        categories.add(new Category(mainActivity.getString(R.string.category_entertainment_tag),mainActivity.getString(R.string.category_entertainment),R.drawable.baseline_local_movies_white_18dp));
+
+        // Setup Categories ListView
+        CategoriesAdapter adapter = new CategoriesAdapter(mainActivity, R.layout.category_list_item, categories);
+        ListView categoriesList = rootView.findViewById(R.id.categories_list_view);
+        categoriesList.setAdapter(adapter);
+        categoriesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                // Retrieve the chosen category title, api category tag
+                String chosenCategoryTitle = categories.get(i).getCategoryTitle();
+                String chosenCategoryTag = categories.get(i).getCategoryApiTag();
+
+                // Pass parameters to ArticlesFragment
+                mainActivity.clickCategory(chosenCategoryTitle, chosenCategoryTag, true);
+            }
+        });
+
+        return rootView;
     }
 
     public MainActivity getMainActivity() {
